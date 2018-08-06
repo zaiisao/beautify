9b0096ed1e8ce6c4d04b2838290fbb3733738539
Jeremy Ahn <me@jeremyahn.com>
2018-08-05 23:28:34 -0400

    creating ref

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+     alert('A name was submitted: ' + this.state.value);
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +26,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <input type="file" id="image" name="image" accept="image/png, image/jpeg" />
-        <input type="file" id="overlay" name="overlay" accept="image/png, image/jpeg" />
-        <input type="submit" />
+        <form onSubmit={this.handleSubmit}>
+          <input type="file" id="image" name="image" accept="image/png, image/jpeg" />
+          <input type="file" id="overlay" name="overlay" accept="image/png, image/jpeg" />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
---
+++ src/UploadForm.js
@@ -0,0 +1,8 @@
+class UploadForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.myRef = React.createRef();
+	}
+
+
+}
