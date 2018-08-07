9fc5ca7dabdafeaae689ec94457766b95c7b41b7
Jeremy Ahn <me@jeremyahn.com>
2018-08-07 17:59:14 -0400

    components work

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UploadForm from './UploadForm.js';
 
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = {value: ''};
+		super(props);
+	}
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-     alert('A name was submitted: ' + this.state.value);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <form onSubmit={this.handleSubmit}>
-          <input type="file" id="image" name="image" accept="image/png, image/jpeg" />
-          <input type="file" id="overlay" name="overlay" accept="image/png, image/jpeg" />
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<UploadForm />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/UploadForm.js
+++ src/UploadForm.js
@@ -1,8 +1,32 @@
+import React, { Component } from 'react';
+
 class UploadForm extends React.Component {
 	constructor(props) {
 		super(props);
 		this.myRef = React.createRef();
+		this.state = {value: ''};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
+	handleChange(event) {
+		this.setState({value: event.target.value});
+	}
 
+	handleSubmit(event) {
+		alert('A name was submitted: ' + this.state.value);
+	}
+
+	render() {
+		return (
+			<form ref={this.myRef} onSubmit={this.handleSubmit}>
+				<input type="file" id="image" name="image" accept="image/png, image/jpeg" />
+				<input type="file" id="overlay" name="overlay" accept="image/png, image/jpeg" />
+				<input type="submit" value="Submit" />
+			</form>
+		);
+	}
 }
+
+export default UploadForm;
